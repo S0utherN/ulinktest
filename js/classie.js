785ba0c544e1ca575c67eaecc949e0289694112e
Yi <75700998+S0utherN@users.noreply.github.com>
2023-07-25 02:24:57 +0100

    up

--- js/classie.js
+++ js/classie.js
@@ -78,19 +78,3 @@
     }
 
 })( window );
-
-// window.onload = function() {
-//     Particles.init({
-//       selector: '.background',
-//       color: ['#DA0463', '#404B69'],
-//         connectParticles: true,
-//         responsive: [{
-//             breakpoint: 800,
-//             options: {
-//                 color: '#00C9B1',
-//                 maxParticles: 80,
-//             connectParticles: false
-//     }
-//   }]
-//     });
-//   };
